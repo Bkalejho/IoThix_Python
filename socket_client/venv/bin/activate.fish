acb8b0db28824eabbf2c1036e7fd80d8180ca16f
bkalejho <bkalejho@gmail.com>
2020-01-24 20:58:13 -0500

    Interaction between socket server and socket client using python

--- socket_client/venv/bin/activate.fish
+++ socket_client/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/alejandro/Io_Thix/IoThix_Python/sockets/venv"
+set -gx VIRTUAL_ENV "/home/alejandro/Io_Thix/IoThix_Python/socket_client/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
